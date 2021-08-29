32820abea9aa3b00395e6bdbfc3fa05f5a26d390
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-08-28 17:31:12 -0700

    Volcanoes.txt was added in for data, looped through lat/lon to add more markers on the map and elev shows elevations for those volcanoes when clicked.

--- map1.py
+++ map1.py
@@ -1,9 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color="green")))
+
+for lt, ln, el in zip(lat, lon, elev):
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
