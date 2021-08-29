2754f84354ca7bc5bbbe27ca12034cda94804169
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-08-29 00:36:50 -0700

    world.json added and used for population layer for map.

--- map1.py
+++ map1.py
@@ -22,6 +22,9 @@ for lt, ln, el in zip(lat, lon, elev):
 	fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(el) + " m", radius=6, fill_color=color_producer(el), 
 	color="grey", fill_opacity=0.7))
 
-map.add_child(fg)
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10000000 
+else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
 
+map.add_child(fg)
 map.save("Map1.html")
